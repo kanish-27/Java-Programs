4a40dbbbf54ffd08843df792722e84c4519037d9
kanish-27 <kanishkrishna.jp2024cse@sece.ac.in>
2025-07-09 11:19:10 +0530

    Add files via upload

---
+++ D_ConstructorExample.java
@@ -0,0 +1,25 @@
+public class D_ConstructorExample {
+	/**
+	 * This is a constructor for the ConstructorExample class.
+	 * It is called when an instance of the class is created.
+	 * Constructors are used to initialize the state of an object.
+	 */
+	D_ConstructorExample()
+	{
+		System.out.println("Constructor Called");
+	}
+
+	/**
+	 * This is the main method, which serves as the entry point for the program.
+	 * It creates an instance of the ConstructorExample class,
+	 * which will invoke the constructor.
+	 */ 
+	public static void main(String[] args)
+	{
+		/**
+		 * Creating an instance of ConstructorExample
+		 * This will call the constructor and print "Constructor Called".
+		 */
+		D_ConstructorExample ce = new D_ConstructorExample();
+	}
+}
